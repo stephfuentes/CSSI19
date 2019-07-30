255f7cf2f65e63d7e15785f7872e5f30f05995ed
stephaniefuentes <stephaniefuentes@mec.sciemce>
2019-07-30 16:42:10 -0400

    Color-Button

---
+++ day6/scripr.js
@@ -0,0 +1,22 @@
+function countToN(x)
+{
+    let absx = Math.abs(x);
+    let index=1;
+    while(index <= absx)
+    {
+        console.log(index);
+        index=index+1; // index++;index+=1
+
+    }
+}
+
+function Multuples(mult,m,n)
+{
+    let absx=Math.abs(n);
+    let index = 1;
+    while(inbex <= absx);
+    { 
+        mult.push(index*m);
+        index=index+1;
+    }
+}
